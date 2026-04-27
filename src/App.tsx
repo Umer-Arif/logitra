e41cc57480bb92a782807f7f53c8d6199f7d3453
madara123pain <161628269+madara123pain@users.noreply.github.com>
2026-04-27 11:56:02 +0500

    feat: add heading and drag-and-drop with border styling

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,13 @@ function App() {
             <h1 className="text-4xl underline bg-blue-500 text-center py-5">
               Logitra
             </h1>
-            <p className="italic text-blue-400 text-center">
+            <p className="italic text-blue-400 text-center pt-5 pb-20">
               Your document engine
             </p>
           </div>
-          <div>
-            <h2 className="text-blue-400 py-30 text-center">Drag or drop</h2>
-          </div>
+          <h2 className="text-2xl text-blue-400 text-center p-10 rounded-2xl border border-blue-400 w-fit mx-auto">
+            Drag or drop
+          </h2>
         </section>
       </div>
     </>
